1a809cd04415724b283fc7cb458d748252eaf3f9
45012010-ui <45012010@parisnanterre.fr>
2026-06-26 17:53:13 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,12 @@ from sqlalchemy.orm import Session
 from . import models
 from .db import engine, SessionLocal
 
+#Imports pour fastapi-health
+import asyncio
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi_health import health
+
 #models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="API principale - Project consultations citoyennes")
@@ -23,18 +29,33 @@ async def root():
     return {"message": "Page par défaut"}
 
 
-@app.get("/health")
-def health(): #Il faut que ceci vérifie si l'API fonctionne correctement (Est-ce qu'il y a une extension qui permet de faire ça?)
-    return{
-        "status": "ok",
-        "module": "M5 API orchestration"
-    }
+# @app.get("/health")
+# def health(): #Il faut que ceci vérifie si l'API fonctionne correctement (Est-ce qu'il y a une extension qui permet de faire ça?)
+#     return{
+#         "status": "ok",
+#         "module": "M5 API orchestration"
+#     }
+#J'ai trouvé une extension appelée fastapi-health qui permet de faire ça, le code en dessous utilise cette extension
 
-@app.post("/semantic-search/search")
-def semantic_search(): #Intégrer à partir de M2 
-    return{
-        "place":"holder"
-    }
+def get_session():
+    return True
+async def is_database_online(session: AsyncSession = Depends(get_session)):
+    try:    
+        await asyncio.wait_for(session.execute("SELECT 1"), timeout=30)
+    except (SQLAlchemyError, TimeoutError): 
+        return False
+    return True
+
+app.add_api_route("/health", health([is_database_online]))
+
+#Fin du code utilisant fastapi_health
+
+# @app.post("/semantic-search/search")
+# def semantic_search(): #Intégrer à partir de M2 
+#     return{
+#         "place":"holder"
+#     }
+#Pas nécessaire, inclure le router de search_api devrait déjà permettre d'accéder à semantic_search
 
 @app.post("/nli/analyze")
 def nli_analyze(): #Intégrer à partir de M3
