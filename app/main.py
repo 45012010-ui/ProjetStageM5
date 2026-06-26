ea090e498090a20311b81e44eb12acd56496e985
45012010-ui <45012010@parisnanterre.fr>
2026-06-26 16:49:15 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 # Démarre l'application
 from fastapi import FastAPI, Depends
+from .src import search_api
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models
 from .db import engine, SessionLocal
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="API principale - Project consultations citoyennes")
+app.include_router(search_api.router)
 
 # Dependency to get a DB session per request
 def get_db():
@@ -16,12 +18,26 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/contributions", response_model=schemas.ContributionRead)
-def create_contribution(contrib: schemas.ContributionCreate, db: Session = Depends(get_db)):
-    db_contrib = models.Contribution(source_id=contrib.source_id, titre=contrib.titre, corps=contrib.corps,
-                                  categorie_id=contrib.categorie_id, distance=contrib.distance,
-                                  similarite=contrib.similarite)
-    db.add(db_contrib)
-    db.commit()
-    db.refresh(db_contrib)
-    return db_contrib
+@app.get("/")
+async def root():
+    return {"message": "Page par défaut"}
+
+
+@app.get("/health")
+def health(): #Il faut que ceci vérifie si l'API fonctionne correctement (Est-ce qu'il y a une extension qui permet de faire ça?)
+    return{
+        "status": "ok",
+        "module": "M5 API orchestration"
+    }
+
+@app.post("/semantic-search/search")
+def semantic_search(): #Intégrer à partir de M2 
+    return{
+        "place":"holder"
+    }
+
+@app.post("/nli/analyze")
+def nli_analyze(): #Intégrer à partir de M3
+    return{
+        "place":"holder"
+    }
